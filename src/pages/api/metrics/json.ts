403cbc7b9770b02efea83ee365eb10bdc0783037
rayhankinan <rayhankinan@gmail.com>
2023-06-14 11:50:45 +0700

    Minor changes to api metrics

--- src/pages/api/metrics/json.ts
+++ src/pages/api/metrics/json.ts
@@ -2,9 +2,13 @@ import { type Metrics } from "@prisma/client/runtime";
 import { type NextApiHandler } from "next";
 import { prisma } from "~/server/db";
 
-const handler: NextApiHandler<Metrics> = async (_, res) => {
-  const metrics = await prisma.$metrics.json();
+const handler: NextApiHandler<Metrics> = async (req, res) => {
+  if (req.method !== "GET") {
+    res.status(405);
+    return;
+  }
 
+  const metrics = await prisma.$metrics.json();
   res.status(200).json(metrics);
 };
 
--- src/pages/api/metrics/prometheus.ts
+++ src/pages/api/metrics/prometheus.ts
@@ -1,9 +1,13 @@
 import { type NextApiHandler } from "next";
 import { prisma } from "~/server/db";
 
-const handler: NextApiHandler<string> = async (_, res) => {
-  const metrics = await prisma.$metrics.prometheus();
+const handler: NextApiHandler<string> = async (req, res) => {
+  if (req.method !== "GET") {
+    res.status(405);
+    return;
+  }
 
+  const metrics = await prisma.$metrics.prometheus();
   res.status(200).send(metrics);
 };
 
